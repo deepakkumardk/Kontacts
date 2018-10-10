5d6c152c7f1c9503a4fd81f068ab164cc6a81346
Deepak Kumar <dpk950@gmail.com>
2018-10-10 13:56:19 +0530

    initial commit

---
+++ app/src/main/java/com/deepak/kontacts/ui/ViewContactActivity.kt
@@ -0,0 +1,28 @@
+package com.deepak.kontacts.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.deepak.kontacts.R
+import kotlinx.android.synthetic.main.activity_view_contact.*
+
+class ViewContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_contact)
+
+        val intent: Intent? = intent
+        val name = intent?.getStringExtra("CONTACT_NAME")
+        val mobile = intent?.getStringArrayListExtra("CONTACT_PHONE")
+        val image = intent?.getBundleExtra("CONTACT_IMAGE")
+
+        view_contact_name.text = name
+        view_contact_phone.text = mobile.toString()
+        Glide.with(this)
+                .load(image)
+                .into(view_contact_image)
+
+    }
+}
